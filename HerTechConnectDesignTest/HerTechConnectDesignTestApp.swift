a63e420480eb8138dae0c0478721aae67f04b75b
Arica Conrad <aricaconrad@outlook.com>
2021-10-30 18:15:01 -0700

    Initial Commit

---
+++ HerTechConnectDesignTest/HerTechConnectDesignTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerTechConnectDesignTestApp.swift
+//  HerTechConnectDesignTest
+//
+//  Created by Student Account on 10/30/21.
+//
+
+import SwiftUI
+
+@main
+struct HerTechConnectDesignTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
